0d3f9ed06e3858bcb77c259ec437ce51275d005c
blake.green <blake.green@5CD2084X2G>
2023-08-31 12:08:36 -0500

    Moving Pillers

--- Main.cpp
+++ Main.cpp
@@ -22,34 +22,61 @@ int main()
 	InitWindow(800, 600, "Not Flappy Bird");
 	SetWindowState(FLAG_VSYNC_HINT);
 
+	Piller piller1;
+	piller1.speed = 200;
+	piller1.x = 700;
+	piller1.y = 0;
+	piller1.height = 600;
+	piller1.width = 100;
+	piller1.speed = 200;
+
+	Piller piller2;
+	piller2.x = 550;
+	piller2.y = 0;
+	piller2.height = 600;
+	piller2.width = 100;
+	piller1.speed = 200;
+
+	Piller piller3;
+	piller3.x = 400;
+	piller3.y = 0;
+	piller3.height = 600;
+	piller3.width = 100;
+	piller1.speed = 200;
+
+	Piller piller4;
+	piller4.x = 250;
+	piller4.y = 0;
+	piller4.height = 600;
+	piller4.width = 100;
+	piller1.speed = 200;
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 			ClearBackground(DARKGREEN);
 
-			Piller piller1;
-			piller1.x = 700;
-			piller1.y = 0;
-			piller1.height = 600;
-			piller1.width = 100;
+			piller1.x -= (700 / piller1.speed);
+			piller2.x -= (700 / piller1.speed);
+			piller3.x -= (700 / piller1.speed);
+			piller4.x -= (700 / piller1.speed);
 
-			Piller piller2;
-			piller2.x = 550;
-			piller2.y = 0;
-			piller2.height = 600;
-			piller2.width = 100;
-
-			Piller piller3;
-			piller3.x = 400;
-			piller3.y = 0;
-			piller3.height = 600;
-			piller3.width = 100;
-
-			Piller piller4;
-			piller4.x = 250;
-			piller4.y = 0;
-			piller4.height = 600;
-			piller4.width = 100;
+			if (piller1.x < -90)
+			{
+				piller1.x = 790;
+			}
+			if (piller2.x < -90)
+			{
+				piller2.x = 790;
+			}
+			if (piller3.x < -90)
+			{
+				piller3.x = 790;
+			}
+			if (piller4.x < -90)
+			{
+				piller4.x = 790;
+			}
 
 			piller1.Draw();
 			piller2.Draw();
