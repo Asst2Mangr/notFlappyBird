e2bea28e94fed2321b4ab4fcb4a0bdf4931c02e3
blake.green <blake.green@5CD2084X2G>
2023-08-31 10:20:38 -0500

    Added Pillers

--- Main.cpp
+++ Main.cpp
@@ -10,7 +10,10 @@ int main()
 		BeginDrawing();
 			ClearBackground(DARKGREEN);
 
-
+			DrawRectangle(700, 0, 100, 600, BLACK);
+			DrawRectangle(550, 0, 100, 600, BLUE);
+			DrawRectangle(400, 0, 100, 600, BLACK);
+			DrawRectangle(250, 0, 100, 600, BLUE);
 			DrawCircle(GetScreenWidth() / 4, GetScreenHeight() / 2, 25, BLACK);
 
 			DrawFPS(10, 10);
