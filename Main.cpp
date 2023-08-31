e259e63da22cb9a96f1ef8f6bfc2094af56cd2fc
blake.green <blake.green@5CD2084X2G>
2023-08-31 11:11:59 -0500

    New Pillers

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,22 @@
 #include "raylib.h"
 
+struct Bird
+{
+
+};
+
+struct Piller
+{
+	float x, y;
+	float height, width;
+	float speed;
+
+	void Draw()
+	{
+		DrawRectangle(x, y, width, height, RED);
+	}
+};
+
 int main()
 {
 	InitWindow(800, 600, "Not Flappy Bird");
@@ -10,10 +27,36 @@ int main()
 		BeginDrawing();
 			ClearBackground(DARKGREEN);
 
-			DrawRectangle(700, 0, 100, 600, BLACK);
-			DrawRectangle(550, 0, 100, 600, BLUE);
-			DrawRectangle(400, 0, 100, 600, BLACK);
-			DrawRectangle(250, 0, 100, 600, BLUE);
+			Piller piller1;
+			piller1.x = 700;
+			piller1.y = 0;
+			piller1.height = 600;
+			piller1.width = 100;
+
+			Piller piller2;
+			piller1.x = 550;
+			piller1.y = 0;
+			piller1.height = 600;
+			piller1.width = 100;
+
+			Piller piller3;
+			piller1.x = 400;
+			piller1.y = 0;
+			piller1.height = 600;
+			piller1.width = 100;
+
+			Piller piller4;
+			piller1.x = 250;
+			piller1.y = 0;
+			piller1.height = 600;
+			piller1.width = 100;
+
+			piller1.Draw();
+			piller2.Draw();
+			piller3.Draw();
+			piller4.Draw();
+
+
 			DrawCircle(GetScreenWidth() / 4, GetScreenHeight() / 2, 25, BLACK);
 
 			DrawFPS(10, 10);
