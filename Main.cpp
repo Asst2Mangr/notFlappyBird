e64919cf4eb3e6fd55fbd9d471ca39b5745cbb59
blake.green <blake.green@5CD2084X2G>
2023-08-31 13:23:25 -0500

    Final moving Pillers (Probably - If I can resist)

--- Main.cpp
+++ Main.cpp
@@ -23,27 +23,26 @@ int main()
 	SetWindowState(FLAG_VSYNC_HINT);
 
 	Piller piller1;
-	piller1.speed = 200;
-	piller1.x = 700;
+	piller1.x = (GetScreenWidth() / 4) + 200;
 	piller1.y = 0;
 	piller1.height = 600;
 	piller1.width = 100;
 	piller1.speed = 5;
 
 	Piller piller2;
-	piller2.x = 550;
+	piller2.x = ((GetScreenWidth() / 4) * 2) + 200;
 	piller2.y = 0;
 	piller2.height = 600;
 	piller2.width = 100;
 
 	Piller piller3;
-	piller3.x = 400;
+	piller3.x = ((GetScreenWidth() / 4) * 3) + 200;
 	piller3.y = 0;
 	piller3.height = 600;
 	piller3.width = 100;
 
 	Piller piller4;
-	piller4.x = 250;
+	piller4.x = ((GetScreenWidth() / 4) * 4) + 200;
 	piller4.y = 0;
 	piller4.height = 600;
 	piller4.width = 100;
