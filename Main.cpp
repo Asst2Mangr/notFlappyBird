4b8c31414aed1eab3a4830d70a1cef6062943cae
blake.green <blake.green@5CD2084X2G>
2023-09-05 11:11:38 -0500

    Piller Kinda Generated

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,7 @@
 #include "raylib.h"
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 //Reaenn, Zoey, Jordan
 // 9/1/2023
 // This is our not flappy bird game!
@@ -30,7 +33,7 @@ struct Piller
 
 	void Draw()
 	{
-		DrawRectangle(x, y - ranMain, width, height, RED);
+		DrawRectangle(x, y + ranMain, width, height, RED);
 		DrawRectangle(x, y + ranMine, width, height, BLUE);
 	}
 };
@@ -39,6 +42,9 @@ int main()
 {
 	InitWindow(800, 600, "Not Flappy Bird");
 	SetWindowState(FLAG_VSYNC_HINT);
+	
+	//srand(time(NULL));
+	int random = rand() % 100;
 
 	Bird bird;
 	bird.x = GetScreenWidth() / 4;
@@ -54,32 +60,32 @@ int main()
 	piller1.height = 600;
 	piller1.width = 100;
 	piller1.speed = 5;
-	piller1.ranMain = 300;
-	piller1.ranMine = piller1.ranMain + piller1.y + 150;
+	piller1.ranMain = GetRandomValue(400, 200);
+	piller1.ranMine = piller1.ranMain - piller1.y - 750;
 
 	Piller piller2;
-	piller2.x = ((GetScreenWidth() / 4) * 2) + 200;
+	piller2.x = (piller1.x + 500);
 	piller2.y = 0;
 	piller2.height = 600;
 	piller2.width = 100;
-	piller2.ranMain = 300;
-	piller2.ranMine = piller2.ranMain + piller2.y + 150;
+	piller2.ranMain = GetRandomValue(400, 200);
+	piller2.ranMine = piller2.ranMain - piller2.y - 750;
 
 	Piller piller3;
-	piller3.x = ((GetScreenWidth() / 4) * 3) + 200;
+	piller3.x = (piller2.x + 500);
 	piller3.y = 0;
 	piller3.height = 600;
 	piller3.width = 100;
-	piller3.ranMain = 300;
-	piller3.ranMine = piller3.ranMain + piller3.y + 150;
+	piller3.ranMain = GetRandomValue(400, 200);
+	piller3.ranMine = piller3.ranMain - piller3.y - 750;
 
 	Piller piller4;
-	piller4.x = ((GetScreenWidth() / 4) * 4) + 200;
+	piller4.x = (piller3.x + 500);
 	piller4.y = 0;
 	piller4.height = 600;
 	piller4.width = 100;
-	piller4.ranMain = 300;
-	piller4.ranMine = piller4.ranMain + piller4.y + 150;
+	piller4.ranMain = GetRandomValue(400, 200);
+	piller4.ranMine = piller4.ranMain - piller4.y - 750;
 
 	while (!WindowShouldClose())
 	{
@@ -93,20 +99,28 @@ int main()
 
 			if (piller1.x < -90)
 			{
-				piller1.x = 790;
+				piller1.x = (piller4.x + 500);
 				piller1.speed += 1* GetFrameTime();
+				piller1.ranMain = GetRandomValue(400, 200);
+				piller1.ranMine = piller1.ranMain - piller1.y - 750;
 			}
 			if (piller2.x < -90)
 			{
-				piller2.x = 790;
+				piller2.x = (piller3.x + 500);
+				piller2.ranMain = GetRandomValue(400, 200);
+				piller2.ranMine = piller2.ranMain - piller2.y - 750;
 			}
 			if (piller3.x < -90)
 			{
-				piller3.x = 790;
+				piller3.x = (piller2.x + 500);
+				piller3.ranMain = GetRandomValue(400, 200);
+				piller3.ranMine = piller3.ranMain - piller3.y - 750;
 			}
 			if (piller4.x < -90)
 			{
-				piller4.x = 790;
+				piller4.x = (piller1.x + 500);
+				piller4.ranMain = GetRandomValue(400, 200);
+				piller4.ranMine = piller4.ranMain - piller4.y - 750;
 			}
 			if (piller1.speed == 0)
 			{
